67bdad7cd0ce263c0f88467312cbc0470133fe64
timcon <nheafy1@gmail.com>
2024-02-02 16:00:25 +0200

    додано перевірку наявності світла

---
+++ src/ton_Saund.cpp
@@ -0,0 +1,68 @@
+
+#pragma once
+#include <main.h>
+
+
+void ton()      {
+ 
+
+ if (flag_Startincubation=1) {
+ // стартова мелодія
+tone(tonePin,392, 350);
+delay(350);
+tone(tonePin,392, 350);
+delay(350);
+tone(tonePin,392, 350);
+delay(350);
+tone(tonePin,311, 250);
+delay(250);
+tone(tonePin,466, 100);
+delay(100);
+tone(tonePin,392, 350);
+
+  tone(tonePin, 440, 250);
+  delay(250);
+  tone(tonePin, 165, 250);
+  delay(250);
+  tone(tonePin, 440, 250);
+  delay(250);
+  delay(250);
+  
+  tone(tonePin, 440, 250);
+  delay(250);
+  tone(tonePin, 165, 250);
+  delay(250);
+  tone(tonePin, 440, 250);
+  delay(250);
+  delay(250);
+ tone(tonePin, 330, 125);
+  delay(125);
+  tone(tonePin, 294, 125);
+  delay(125);
+  tone(tonePin, 262, 125);
+  delay(124);
+  tone(tonePin, 494, 125);
+  delay(125);
+  tone(tonePin, 440, 125);
+  delay(125);
+  tone(tonePin, 494, 125);
+  delay(125);
+  tone(tonePin, 262, 125);
+  delay(125);
+  tone(tonePin, 294, 125);
+  delay(125);
+  
+  tone(tonePin, 330, 250);
+  delay(250);
+  tone(tonePin, 165, 250);
+  delay(250);
+  tone(tonePin, 440, 250);
+  delay(250);
+ }
+  else if (flag_power)
+  {
+    /* code */
+  }
+  
+  
+}
